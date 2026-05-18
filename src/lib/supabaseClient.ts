b67daecfb097581aba5991b774e78ff719aae83d
Mangiafesto Electronics LLC <ZacharyMangiafesto@MangiafestoElectronics.onmicrosoft.com>
2026-05-18 16:49:47 -0400

    Fix Supabase browser client import for build

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,9 +1,9 @@
 'use client';
 
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 
 export const supabaseClient = () =>
-    createBrowserClient(
+    createPagesBrowserClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     );
