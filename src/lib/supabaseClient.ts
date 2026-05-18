e6aa5efef58385c05cc7fa668f220c1bce334358
Mangiafesto Electronics LLC <ZacharyMangiafesto@MangiafestoElectronics.onmicrosoft.com>
2026-05-18 16:52:54 -0400

    Fix Supabase client call signature

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -3,7 +3,4 @@
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 
 export const supabaseClient = () =>
-    createPagesBrowserClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    );
+    createPagesBrowserClient();
